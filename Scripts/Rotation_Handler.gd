126352ac0217ddfbd99a5008d98e1da82c870766
Lucas-FAlves <Lucas.3451@hotmail.com>
2023-09-30 20:19:49 -0300

    Personagem já segue a câmera

--- Scripts/Rotation_Handler.gd
+++ Scripts/Rotation_Handler.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CollisionShape2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +8,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	rotate(get_angle_to(get_global_mouse_position()) + 1.5708)
+	#look_at(get_global_mouse_position())
