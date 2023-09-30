3bbd5fc5cef765246ca3b27128065afb5578d92b
Lucas-FAlves <Lucas.3451@hotmail.com>
2023-09-30 20:20:20 -0300

    nada

--- Scripts/Rotation_Handler.gd
+++ Scripts/Rotation_Handler.gd
@@ -1,12 +1,5 @@
 extends CollisionShape2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rotate(get_angle_to(get_global_mouse_position()) + 1.5708)
 	#look_at(get_global_mouse_position())
