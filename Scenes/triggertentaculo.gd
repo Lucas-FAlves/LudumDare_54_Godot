14a07ca935d823a640241943474c6faef069e0c4
Lucas-FAlves <Lucas.3451@hotmail.com>
2023-10-02 02:18:52 -0300

    Ajeitar Anim tentaculo ainda

--- Scenes/triggertentaculo.gd
+++ Scenes/triggertentaculo.gd
@@ -3,7 +3,9 @@ extends Area2D
 const pushStrength = 10  # Força do empurrão
 @export var stunDuration: float = 1.0  # Duração do atordoamento em segundos
 @onready var player = get_node("/root/Level/Player_Character")
+@onready var hitArea: CanvasItem = $CollisionShape2D/Sprite2D
 var playerInArea = false
+var time_to_attack = 3
 
 func _process(delta):
 	pass
@@ -17,14 +19,16 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.name == "Player_Character":  # Substitua pelo nome correto do jogador
 		playerInArea = true
+		hitArea.visible = true
+		$CollisionShape2D/Sprite2D/Timer.wait_time = time_to_attack
+		$CollisionShape2D/Sprite2D/Timer.start()
 		#body.set_physics_process(false)  # Impedir o movimento do jogador
-		$AnimatedSprite2D.play("Attack",2)
-		$Timer.wait_time = 10
-		$Timer.start()
+		
 
 func _on_timer_timeout():
 	playerInArea= false
 	player.set_physics_process(true)
+	hitArea.visible=false
 	#if attacked == false:
 		#if playerInArea == true:
 			#player.set_physics_process(false)
@@ -34,9 +38,11 @@ func _on_timer_timeout():
 func _on_animated_sprite_2d_animation_finished():
 	if $AnimatedSprite2D.animation=="Attack":
 		if playerInArea == true:
+			playerInArea=false
 			player.set_physics_process(false)
 			$Timer.wait_time = stunDuration
 			$Timer.start()
+		$AnimatedSprite2D.stop()
 		$AnimatedSprite2D.play("Spawn", true)
 		#attacked = false
 
@@ -51,3 +57,8 @@ func _on_body_exited(body):
 	#	$Timer.wait_time = stunDuration
 	#	$Timer.start()
 #func _on_Area2D_body_entered(body):
+
+func _on_timer_Canva_timeout():
+	$AnimatedSprite2D.play("Attack",2)
+	$Timer.wait_time = 10
+	$Timer.start()	 # Replace with function body.
